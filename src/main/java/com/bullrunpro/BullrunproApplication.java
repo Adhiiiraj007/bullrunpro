19747e31cfb62cafb11b3e8ae1954fe2bd0f82a4
Adhiiiraj007 <adhirajsambhajishinde007@gmail.com>
2026-02-14 15:27:37 +0530

    Disabled datasource auto configuration

--- src/main/java/com/bullrunpro/BullrunproApplication.java
+++ src/main/java/com/bullrunpro/BullrunproApplication.java
@@ -2,12 +2,12 @@ package com.bullrunpro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class BullrunproApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BullrunproApplication.class, args);
 	}
-
 }
