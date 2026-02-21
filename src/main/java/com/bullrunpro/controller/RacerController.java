b217fdb58047729b76e7f29bce4bb14db24113e9
Adhiiiraj007 <adhirajsambhajishinde007@gmail.com>
2026-02-22 04:44:01 +0530

    Added Razorpay payment integration

--- src/main/java/com/bullrunpro/controller/RacerController.java
+++ src/main/java/com/bullrunpro/controller/RacerController.java
@@ -2,6 +2,10 @@ package com.bullrunpro.controller;
 
 import com.bullrunpro.entity.Racer;
 import com.bullrunpro.repository.RacerRepository;
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +19,20 @@ public class RacerController {
         this.racerRepository = racerRepository;
     }
 
+    // Razorpay Keys from application.properties
+    @Value("${razorpay.key.id}")
+    private String keyId;
+
+    @Value("${razorpay.key.secret}")
+    private String keySecret;
+
     // Home Page
     @GetMapping("/")
     public String home() {
         return "index";
     }
 
-    // Show Registration Form
+    // Registration Form
     @GetMapping("/registration")
     public String showForm(Model model) {
         model.addAttribute("racer", new Racer());
@@ -35,10 +46,27 @@ public class RacerController {
         return "success";
     }
 
-    // View All Racers
+    // View Racers
     @GetMapping("/racers")
     public String viewRacers(Model model) {
         model.addAttribute("racers", racerRepository.findAll());
         return "racers";
+    }
+
+    // Create Razorpay Order
+    @PostMapping("/create-order")
+    @ResponseBody
+    public String createOrder(@RequestParam int amount) throws Exception {
+
+        RazorpayClient client = new RazorpayClient(keyId, keySecret);
+
+        JSONObject options = new JSONObject();
+        options.put("amount", amount * 100);
+        options.put("currency", "INR");
+        options.put("receipt", "txn_123456");
+
+        Order order = client.orders.create(options);
+
+        return order.toString();
     }
 }
