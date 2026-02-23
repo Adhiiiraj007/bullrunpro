e6f50b169906c314284a401a3048366c16fabbb6
Adhiiiraj007 <adhirajsambhajishinde007@gmail.com>
2026-02-23 05:57:36 +0530

    Update RacerController

--- src/main/java/com/bullrunpro/controller/RacerController.java
+++ src/main/java/com/bullrunpro/controller/RacerController.java
@@ -61,7 +61,7 @@ public class RacerController {
     @ResponseBody
     public Order createOrder() throws Exception {
 
-        RazorpayClient client = new RazorpayClient("rzp_test_SJJU7HTqnGA6VF", "rzp_test_SJJU7HTqnGA6VF");
+        RazorpayClient client = new RazorpayClient("rzp_test_SJO5P1R4CJbMP9", "DpxSzi7mZCQHBcyeKlXmQao9");
 
         JSONObject options = new JSONObject();
         options.put("amount", 1500);
